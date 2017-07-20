4e2b08bfbc93864b742b63cb070b4acb33011e73
kura5 <laboradian@gmail.com>
2017-07-20 16:57:47 +0900

    redux-devtools を組み込んでみた。

--- src/js/main.js
+++ src/js/main.js
@@ -8,21 +8,39 @@ require('file-loader?name=../../dist/[name].[ext]!../index.html');
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { applyMiddleware, createStore } from 'redux'
+import { applyMiddleware, createStore, compose } from 'redux'
 import logger from 'redux-logger'
 import calcApp from './reducers'
 import Addition from './containers/Addition'
 import Multiplication from './containers/Multiplication'
+import DevTools from './containers/DevTools';
 
 // store は1つだけ使う
-const store = createStore(
-  calcApp,
-  applyMiddleware(logger)
-)
+//const store = createStore(
+//  calcApp,
+//  applyMiddleware(logger)
+//)
+const store = (() => {
+
+  const enhancer = compose(
+    // Middleware you want to use in development:
+    applyMiddleware(logger),
+    // Required! Enable Redux DevTools with the monitors you chose
+    DevTools.instrument()
+  );
+
+  return createStore(
+    calcApp,
+    enhancer
+  );
+})()
+
 
 render(
   <Provider store={store}>
-    <Addition />
+    <div>
+      <Addition />
+    </div>
   </Provider>,
   document.getElementById('root-addition')
 )
